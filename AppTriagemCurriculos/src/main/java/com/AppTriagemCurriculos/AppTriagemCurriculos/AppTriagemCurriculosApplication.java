f6061a128246c5663e07a260d2a75d56801ff47b
JopeCys <167272228+JopeCys@users.noreply.github.com>
2024-08-06 09:53:51 -0300

    Init

---
+++ AppTriagemCurriculos/src/main/java/com/AppTriagemCurriculos/AppTriagemCurriculos/AppTriagemCurriculosApplication.java
@@ -0,0 +1,13 @@
+package com.AppTriagemCurriculos.AppTriagemCurriculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTriagemCurriculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTriagemCurriculosApplication.class, args);
+	}
+
+}
---
+++ AppTriagemCurriculos/src/test/java/com/AppTriagemCurriculos/AppTriagemCurriculos/AppTriagemCurriculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AppTriagemCurriculos.AppTriagemCurriculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTriagemCurriculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
